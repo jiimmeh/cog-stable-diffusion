de0dbda705c85f5d76bfe99adec4e315f719bc6e
jimmy-desenio <76520224+jimmy-desenio@users.noreply.github.com>
2023-03-04 17:38:40 +0100

    Download weights changed

--- predict.py
+++ predict.py
@@ -17,7 +17,7 @@ from diffusers.pipelines.stable_diffusion.safety_checker import (
 )
 
 
-MODEL_ID = "stabilityai/stable-diffusion-2-1"
+MODEL_ID = "JimDesenio/desenio-landscapes"
 MODEL_CACHE = "diffusers-cache"
 SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 
