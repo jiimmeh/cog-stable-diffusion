0914e0f46f84fa8638a9c8f87d3c30c2143df04c
jimmy-desenio <76520224+jimmy-desenio@users.noreply.github.com>
2023-03-04 18:15:01 +0100

    Change everything to new model

--- predict.py
+++ predict.py
@@ -174,31 +174,31 @@ class Predictor(BasePredictor):
 def make_scheduler(name):
     return {
         "PNDM": PNDMScheduler.from_config(
-            "runwayml/stable-diffusion-v1-5",
+            "JimDesenio/desenio-landscapes",
             cache_dir=MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
         ),
         "K-LMS": LMSDiscreteScheduler.from_config(
-            "runwayml/stable-diffusion-v1-5",
+            "JimDesenio/desenio-landscapes",
             cache_dir=MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
         ),
         "DDIM": DDIMScheduler.from_config(
-            "runwayml/stable-diffusion-v1-5",
+            "JimDesenio/desenio-landscapes",
             cache_dir=MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
         ),
         "K_EULER": EulerDiscreteScheduler.from_config(
-            "runwayml/stable-diffusion-v1-5",
+            "JimDesenio/desenio-landscapes",
             cache_dir=MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
         ),
         "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(
-            "runwayml/stable-diffusion-v1-5",
+            "JimDesenio/desenio-landscapes",
             cache_dir=MODEL_CACHE,
             local_files_only=True,
             subfolder="scheduler",
